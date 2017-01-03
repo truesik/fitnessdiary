d4d05a767342174b3cd4d6d43de846edf3ada5a8
truesik <truesik@gmail.com>
2017-01-03 20:52:04 +0300

    single exercise set component

---
+++ src/app/diary/exercise/exercise-set/exercise-set.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise-set',
+  templateUrl: './exercise-set.component.html',
+  styleUrls: ['./exercise-set.component.css']
+})
+export class ExerciseSetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
