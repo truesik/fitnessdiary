60488121d541048430104925f40e623d09325762
truesik <truesik@gmail.com>
2017-01-07 23:56:58 +0300

    express api routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,43 +1,79 @@
-// const express = require('express');
-import express from "express";
-// import * as models from '../models';
+var express = require('express');
 var models = require('../models');
 
-const router = express.Router();
+var router = express.Router();
 
-/* GET api listing. */
-router.get('/api/diaries', (req, res) => {
-  // res.send('api works');
-  models.Diary.findAll().then(diaries => console.log(diaries));
+/* DIARIES */
+router.get('/diaries', (req, res) => {
+  models.Diary.findAll()
+    .then(diaries => res.json(diaries))
+    .catch(error => res.send(error));
 });
 
-router.post('/api/diaries', (req, res) => {
-  var title = req.body.title;
-  var startDate = req.body.startDate;
-  var description = req.body.description;
+router.post('/diaries', (req, res) => {
   models.Diary.create({
-    title,
-    startDate,
-    description
-  }).then(diary => console.log(diary))
-    .catch(error => console.log(error));
+    title: req.body.title,
+    startDate: req.body.startDate,
+    description: req.body.description
+  }).then(diary => res.status(201).json(diary))
+    .catch(error => res.json(error));
 });
 
-router.post('/api/diaries/:diaryId', (req, res) => {
-  var diaryId = req.params.diaryId;
-  models.Diary.findById(diaryId)
-    .then(diary => console.log(diary))
-    .catch(error => console.log(error));
+router.get('/diaries/:diaryId', (req, res) => {
+  models.Diary.findOne({
+    include: [models.Exercise],
+    where: {
+      id: req.params.diaryId
+    }
+  }).then(diary => res.json(diary))
+    .catch(error => res.json(error));
 });
 
-router.delete('/api/diaries/:diaryId', (req, res) => {
-  var diaryId = req.params.diaryId;
+router.delete('/diaries/:diaryId', (req, res) => {
   models.Diary.destroy({
     where: {
-      id: diaryId
+      id: req.params.diaryId
     }
-  }).then(() => console.log('done'))
-    .catch(error => console.log(error))
+  }).then(diary => res.json(diary))
+    .catch(error => res.json(error))
+});
+
+/* EXERCISES */
+router.post('/exercises', (req, res) => {
+  models.Exercise.findAll({
+    where: {
+      diary_id: req.body.id
+    }
+  }).then(exercises => res.json(exercises))
+    .catch(error => res.json(error))
+});
+
+router.post('/exercises', (req, res) => {
+  models.Exercise.create({
+    title: req.body.title,
+    muscleGroup: req.body.muscleGroup,
+    date: req.body.date,
+    diary_id: req.body.diary_id
+  })
+});
+
+router.get('/exercises/:exerciseId', (req, res) => {
+  models.Exercise.findOne({
+    include: [models.ExerciseSet],
+    where: {
+      id: req.params.exerciseId
+    }
+  }).then(exercise => res.json(exercise))
+    .catch(error => res.json(error))
+});
+
+router.delete('/execises/:exerciseId', (req, res) => {
+  models.Exercise.destroy({
+    where: {
+      id: req.params.exerciseId
+    }
+  }).then(exercise => res.json(exercise))
+    .catch(error => res.json(error))
 });
 
 module.exports = router;
