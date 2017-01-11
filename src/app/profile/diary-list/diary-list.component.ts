4097bdf4b9eabfd3bc79c886bc063188ec1e4869
truesik <truesik@gmail.com>
2017-01-11 20:24:36 +0300

    diary list

--- src/app/profile/diary-list/diary-list.component.ts
+++ src/app/profile/diary-list/diary-list.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DiaryService } from '../../shared/diary.service';
+import Diary from '../../shared/diary.model';
+
 @Component({
   selector: 'app-diary-list',
   templateUrl: './diary-list.component.html',
   styleUrls: ['./diary-list.component.css']
 })
 export class DiaryListComponent implements OnInit {
+  diaries: Diary[] = [];
+  error;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private diaryService: DiaryService) {
   }
 
+  ngOnInit(): void {
+    this.getDiaries();
+  }
+
+  getDiaries(): void {
+    this.diaryService
+      .getAllDiaries()
+      .subscribe(
+        (diaries: Diary[]) => this.diaries = diaries,
+        error => this.error = error
+      );
+  }
+
+  addDiary(newDiary: Diary): void {
+    this.diaryService
+      .addDiary(newDiary)
+      .subscribe(
+        (diary: Diary) => this.diaries.push(diary),
+        error => this.error = error
+      );
+  }
+
+  removeDiary(diary: Diary): void {
+    this.diaryService
+      .removeDiary(diary)
+      .subscribe(
+        () => {
+          const index = this.diaries.indexOf(diary);
+          this.diaries.splice(index, 1);
+        },
+        error => this.error = error);
+  }
 }
