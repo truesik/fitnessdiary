3ef77fb1497285f9215cb90be651d06083a5e51b
truesik <truesik@gmail.com>
2017-01-11 20:24:13 +0300

    diary form

---
+++ src/app/profile/diary-list/diary-form/diary-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import Diary from '../../../shared/diary.model';
+
+@Component({
+  selector: 'app-diary-form',
+  templateUrl: './diary-form.component.html',
+  styleUrls: ['./diary-form.component.css']
+})
+export class DiaryFormComponent {
+  @Output() addDiary = new EventEmitter();
+
+  add(title: string): void {
+    let newDiary = new Diary();
+    newDiary.title = title;
+    newDiary.startDate = new Date();
+    newDiary.description = 'some description';
+    this.addDiary.emit(newDiary);
+  }
+}
