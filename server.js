dfae3d49d48b4fa072414aed0cdbca1312abd105
truesik <truesik@gmail.com>
2017-01-18 23:05:00 +0300

    add passport configs

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const models = require('./server/models');
+const LocalStrategy = require('passport-local').Strategy;
+const cookieParser = require('cookie-parser');
 
 // Get our API routes
 const api = require('./server/routes/api');
@@ -12,6 +17,37 @@ const app = express();
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+// Authentication
+app.use(session({
+  secret: 'GSD5346sdS7gDSjlsdjgSDg#$!(L',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Passport config
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+  models.user.findById(id)
+    .then(user => done(null, user))
+    .catch(error => done(error));
+});
+
+passport.use('login', new LocalStrategy((username, password, done) => {
+  models.user.findOne({
+    where: {
+      username,
+      password
+    }
+  }).then(user => done(null, user))
+    .catch(error => done(error));
+}));
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
