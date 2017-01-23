c90fea79eb124a70bcf0dfd6692126c59a9aca6b
truesik <truesik@gmail.com>
2017-01-23 23:36:55 +0300

    authenticate service

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -5,23 +5,46 @@ import User from './user.model';
 
 @Injectable()
 export class AuthService {
+  private loggedIn: boolean = false;
+
   constructor(private http: Http) {
   }
 
   login(userCredentials) {
     let headers = new Headers({'Content-Type': 'application/json'});
     let options = new RequestOptions({headers: headers});
-    this.http.post('/api/login', userCredentials, options)
+    return this.http.post('/api/login', userCredentials, options)
       .map((response: Response) => response.json())
-      .map(json => new User(json))
-      .catch((error: Response) => Observable.throw(error.json()));
+      .map(json => {
+        this.loggedIn = true;
+        return new User(json);
+      })
+      .catch((error: Response) => Observable.throw(new Error(`${error.status} ${error.statusText}`)));
   }
 
   logout() {
     let headers = new Headers({'Content-Type': 'application/json'});
     let options = new RequestOptions({headers: headers});
-    this.http.post('/api/login', {}, options)
-      .map((response: Response) => response)
-      .catch((error: Response) => Observable.throw(error.json()));
+    return this.http.post('/api/logout', {}, options)
+      .map((response: Response) => response.json())
+      .map(json => {
+        json.success ? this.loggedIn = false : this.loggedIn = true;
+        return json.success;
+      })
+      .catch((error: Response) => Observable.throw(new Error(`${error.status} ${error.statusText}`)));
+  }
+
+  checkLoggedIn(): Observable<boolean> {
+    return this.http.get('/api/loggedIn')
+      .map((response: Response) => response.json())
+      .map(json => {
+        json.isLoggedIn ? this.loggedIn = true : this.loggedIn = false;
+        return json.isLoggedIn;
+      })
+      .catch((error: Response) => Observable.throw(new Error(`${error.status} ${error.statusText}`)));
+  }
+
+  isLoggedIn(): boolean {
+    return this.loggedIn;
   }
 }
