a556bfbf7424d91610cd92e0ace7c77fdf3cfec9
truesik <truesik@gmail.com>
2017-01-24 20:25:45 +0300

    add auth routes, and and auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { DiaryComponent } from './diary/diary.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { AuthGuardService } from './shared/auth-guard.service';
 
 export const ROUTES: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'profile', component: ProfileComponent},
-  {path: 'profile/diaries/:diaryId', component: DiaryComponent}
+  {path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService]},
+  {path: 'profile/diaries/:diaryId', component: DiaryComponent, canActivate: [AuthGuardService]},
+  {path: 'login', component: SignInComponent},
+  {path: 'register', component: SignUpComponent}
 ];
